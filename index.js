26d49e4dcd288b7769132e148aab45c5c5745936
Benjamin Loustau <benloustau@gmail.com>
2015-07-28 20:23:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+var myModule = angular.module('UITHEMEPICKER', []);
+
+myModule.controller('MyController', function(){
+	var self = this;
+
+self.colors = [{
+
+name:"Red",
+backgroundcolor: "red",
+folders: "green",
+item: "blue",
+
+}, {
+
+name:"Green",
+backgroundcolor: "green",
+folders: "red",
+item: "blue",
+
+}, {
+
+name:"Blue",
+backgroundcolor: "blue",
+folders: "green",
+item: "red",
+
+}, {
+
+name:"Yellow",
+backgroundcolor: "yellow",
+folders: "pink",
+item: "gray",
+
+}, {
+
+name:"None",
+backgroundcolor: "white",
+folders: "white",
+item: "white",
+
+}];
+
+self.selection = self.colors[4];
+
+
+self.folders = [{
+
+	name: "Grocery",
+
+},{
+
+	name: "Vacation",
+
+},{
+
+	name: "Angular",
+
+}];
+
+self.addItemArray = [];
+
+self.addItem = function(addItemObject) {
+	self.addItemArray.push(addItemObject);
+	self.addItemObject = {};
+};
+
+
+});
